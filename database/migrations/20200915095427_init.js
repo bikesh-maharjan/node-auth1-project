36ac8e1de3cbc2e96a1b5830dbd7d658b6e9445c
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-15 09:59:15 -0700

    created a table

--- database/migrations/20200915095427_init.js
+++ database/migrations/20200915095427_init.js
@@ -1,8 +1,23 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("roles", (tbl) => {
+      tbl.increments();
+      tbl.string("name", 128).notNullable().unique();
+    })
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 128).notNullable().unique().index();
+      tbl.string("password", 255).notNullable();
 
-exports.up = function(knex) {
-  
+      tbl
+        .integer("role")
+        .unsigned()
+        .references("roles.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("roles").dropTableIfExists("users");
 };
