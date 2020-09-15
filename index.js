1d5b1da83f614d32c0718c5d614ea7bd62133ad7
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-15 09:22:22 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotnev").config();
+
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 5001;
+
+server.listen(port, () => console.log(`\n** Running on port ${port}**\n`));
